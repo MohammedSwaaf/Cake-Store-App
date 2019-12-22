2b617649c8664fe3f84411022fdc81caec4e6d39
Mohammed Swaaf <mohammedswaaf@gmail.com>
2019-12-22 18:18:02 +0200

    add logger for applyMiddleWare to see the logs of actions

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,7 +1,8 @@
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import rootReducer from './roorReducers';
+import logger from 'redux-logger';
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(logger));
 
 export default store;
